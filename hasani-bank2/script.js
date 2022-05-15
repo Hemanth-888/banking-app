01e525eff255e0ebc6cbf150547790827ef3c864
Juwan H <63868021+Juwan-Hollingsworth@users.noreply.github.com>
2022-05-15 19:57:39 -0400

    Implement Lazy Loading Imgs

--- hasani-bank2/script.js
+++ hasani-bank2/script.js
@@ -156,6 +156,30 @@ allSections.forEach(function (section) {
   section.classList.add("section--hidden");
 });
 /////////////////////////////////////////////
+//Lazy loading images
+
+const imgTargets = document.querySelectorAll("img[data-src]");
+const loadImg = function (entries, observer) {
+  const [entry] = entries;
+
+  if (!entry.isIntersecting) return;
+
+  //replace src w data src
+  entry.target.src = entry.target.dataset.src;
+
+  entry.target.addEventListener("load", function () {
+    entry.target.classList.remove("lazy-img");
+  });
+
+  observer.unobserve(entry.target);
+};
+const imgObserver = new IntersectionObserver(loadImg, {
+  root: null,
+  threshold: 0,
+  rootMargin: "200px",
+});
+
+imgTargets.forEach((img) => imgObserver.observe(img));
 
 /////////////////////////////////////////////
 
